87c839a27f7936f4d932261aaf338870679414c2
austrez <austrez@yandex.com>
2022-09-13 21:19:56 +0300

    add posgres

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,8 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-	imports: [UsersModule],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: `.${process.env.NODE_ENV}.env`,
+		}),
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: process.env.POSTGRES_HOST,
+			port: Number(process.env.POSTGRES_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+			models: [],
+			autoLoadModels: true,
+		}),
+		UsersModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
